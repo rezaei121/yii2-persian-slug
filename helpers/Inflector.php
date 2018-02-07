acd234ec86b5f06dc6dbed3c704a1d39a38def50
Ehsan Rezaei <mr.rezaee121@gmail.com>
2018-02-07 15:48:08 +0330

    init

---
+++ helpers/Inflector.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace developit\persianSlug\helpers;
+
+class Inflector extends \yii\helpers\Inflector
+{
+    /**
+     * Returns a string with all spaces converted to given replacement,
+     * non word characters removed and the rest of characters transliterated.
+     *
+     * If intl extension isn't available uses fallback that converts latin characters only
+     * and removes the rest. You may customize characters map via $transliteration property
+     * of the helper.
+     *
+     * @param string $string An arbitrary string to convert
+     * @param string $replacement The replacement to use for spaces
+     * @param bool $lowercase whether to return the string in lowercase or not. Defaults to `true`.
+     * @return string The converted string.
+     */
+    public static function slug($string, $replacement = '-', $lowercase = true)
+    {
+        $string = static::transliterate($string);
+        $string = preg_replace('/[^a-zA-Z0-9=\s—–-]+/u', '', $string);
+        $string = preg_replace('/[=\s—–-]+/u', $replacement, $string);
+        $string = trim($string, $replacement);
+
+        return $lowercase ? strtolower($string) : $string;
+    }
+}
